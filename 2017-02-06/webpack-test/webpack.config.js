d9e9ceaea449666677ffbdceca822e2c5ef31af5
Mike Funk <mike@mikefunk.com>
2017-02-06 23:46:25 -0800

    today

---
+++ 2017-02-06/webpack-test/webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'build.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015',
+      },
+    ],
+  },
+};
