416e072c9186c4f8db1b703ba8a93ab45423871a
Mike Funk <mike@mikefunk.com>
2017-02-12 13:19:51 -0800

    added hot reloading

--- 2017-02-11/vue-test/webpack.config.js
+++ 2017-02-11/vue-test/webpack.config.js
@@ -3,18 +3,28 @@ const path = require('path');
 module.exports = {
   entry: path.join(__dirname, 'src/main'),
   output: {
-    filename: '[name].js',
+    filename: '[name].js?[hash]',
     path: path.join(__dirname, 'build'),
+    publicPath: 'build/',
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
         loader: 'vue-loader',
-        query: {
-          presets: [
-            ['flow-vue'],
-          ],
+        // query: {
+        //   presets: [
+        //     ['flow-vue'],
+        //   ],
+        // },
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            scss: 'vue-style-loader!css-loader!sass-loader',
+            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+          },
         },
       },
     ],
@@ -23,5 +33,11 @@ module.exports = {
     alias: {
       vue$: 'vue/dist/vue.common.js',
     },
+  },
+  devServer: {
+    open: true,
+    inline: true,
+    hot: true,
+    compress: true,
   },
 };
