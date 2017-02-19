13d02302f35211dd3918f9a76d22ed180c60f16f
Mike Funk <mike@mikefunk.com>
2017-02-19 00:49:22 -0800

    working

--- 2017-02-18/webpack-test/app.js
+++ 2017-02-18/webpack-test/app.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: off */
 import Test from './Test'
+import styles from './app.scss'
 
 new Test()
--- 2017-02-18/webpack-test/webpack.config.js
+++ 2017-02-18/webpack-test/webpack.config.js
@@ -12,6 +12,11 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: 'style-loader!css-loader!sass-loader'
       }
     ]
   }
